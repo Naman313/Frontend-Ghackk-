f80e85204c4f58e089c7b6f89458f14df4fc3a3a
Naman313 <2021io12na@mitsgwl.ac.in>
2024-10-15 17:38:07 +0530

    hi

--- src/pages/login.js
+++ src/pages/login.js
@@ -13,7 +13,7 @@ export default function Login() {
   
     if (username && password) {
       try {
-        const response = await fetch('https://backend-ghackk-1.onrender.com/api/auth/login', {
+        const response = await fetch('https://backend-ghackk-1.onrender.com/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
